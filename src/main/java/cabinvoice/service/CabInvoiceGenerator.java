55afac3fa4b19b24a1cac8b13d7b5476874868a6
“Kaustavi <kaustavisen8@gmail.com>
2020-07-10 19:46:54 +0530

    Added:ability to calculate fare

--- src/main/java/cabinvoice/service/CabInvoiceGenerator.java
+++ src/main/java/cabinvoice/service/CabInvoiceGenerator.java
@@ -1,7 +1,9 @@
 package cabinvoice.service;
 
 public class CabInvoiceGenerator {
+    private static final int FARE_PER_KILOMETER = 10 ;
+    private static final int FARE_PER_MINUTE = 1 ;
     public double fareCalculator(double distance, int time) {
-        return 0;
+        return distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
     }
 }
