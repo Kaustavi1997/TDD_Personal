d470c1711700d8487113b2a3788035ca42e04619
“Kaustavi <kaustavisen8@gmail.com>
2020-07-10 20:37:05 +0530

    Added:declaration of enum and method

--- src/main/java/cabinvoice/service/CabInvoiceGenerator.java
+++ src/main/java/cabinvoice/service/CabInvoiceGenerator.java
@@ -1,10 +1,17 @@
 package cabinvoice.service;
+import cabinvoice.utility.UserCategory;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CabInvoiceGenerator {
     private static final int FARE_PER_KILOMETER = 10 ;
     private static final int FARE_PER_MINUTE = 1 ;
     private static final double MINIMUM_FARE = 5.0;
-    public double fareCalculator(double distance, int time) {
+    private Map<String, InvoiceSummary> invoiceSummaryMap;
+    public CabInvoiceGenerator(){
+        invoiceSummaryMap = new HashMap<String, InvoiceSummary>();
+    }
+    public double fareCalculator(double distance, int time, UserCategory category) {
         double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
         return Math.max(fare, MINIMUM_FARE);
     }
@@ -12,14 +19,16 @@ public class CabInvoiceGenerator {
     public InvoiceSummary totalFareCalculator(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride:rides ){
-            totalFare += this.fareCalculator(ride.distance, ride.time);
+            totalFare += this.fareCalculator(ride.distance, ride.time, UserCategory.NORMAL);
         }
         return new InvoiceSummary(rides.length, totalFare);
     }
-    public void setUserSpecificInvoice(Ride[] userRides, String user) { }
+    public void setUserSpecificInvoice(Ride[] userRides, String user, UserCategory premium) {
+        invoiceSummaryMap.put(user, this.totalFareCalculator(userRides));
+    }
 
-    public InvoiceSummary getUserInvoiceSummary(String userName) {
-        return null;
+    public InvoiceSummary getUserInvoiceSummary(String userID) {
+        return invoiceSummaryMap.get(userID);
     }
 }
 
---
+++ src/main/java/cabinvoice/utility/UserCategory.java
@@ -0,0 +1,5 @@
+package cabinvoice.utility;
+
+public enum UserCategory {
+    PREMIUM,NORMAL
+}
