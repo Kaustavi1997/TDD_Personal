f2a7a4de9bb201a831440b29983a4512b9660ad6
“Kaustavi <kaustavisen8@gmail.com>
2020-07-10 20:40:47 +0530

    Added:testcase for validation of premium fares and refactor the testcases

--- src/test/java/cabinvoiceTest/CabInvoiceGeneratorTest.java
+++ src/test/java/cabinvoiceTest/CabInvoiceGeneratorTest.java
@@ -1,5 +1,6 @@
 package cabinvoiceTest;
 import cabinvoice.service.CabInvoiceGenerator;
+import cabinvoice.utility.UserCategory;
 import cabinvoice.service.InvoiceSummary;
 import cabinvoice.service.Ride;
 import org.junit.Assert;
@@ -15,12 +16,12 @@ public class CabInvoiceGeneratorTest {
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 3.0;
         int time = 2;
-        double fare = cabInvoiceGenerator.fareCalculator(distance, time);
+        double fare = cabInvoiceGenerator.fareCalculator(distance, time, UserCategory.NORMAL);
         Assert.assertEquals(32, fare, 0.0);
     }
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare(){
-        double fare = cabInvoiceGenerator.fareCalculator(0.1,1);
+        double fare = cabInvoiceGenerator.fareCalculator(0.1,1, UserCategory.NORMAL);
         Assert.assertEquals(5, fare, 0.0);
     }
     @Test
@@ -35,11 +36,22 @@ public class CabInvoiceGeneratorTest {
     public void givenUserId_ShouldReturnInvoiceOfGivenUserId() {
         Ride[] user1Rides = { new Ride(3.0, 2),
                 new Ride(0.1, 1)};
-        cabInvoiceGenerator.setUserSpecificInvoice(user1Rides, "User1");
+        cabInvoiceGenerator.setUserSpecificInvoice(user1Rides, "User1", UserCategory.NORMAL);
         Ride []user2Rides = {new Ride(5.0, 2),
                 new Ride(6.1, 3)};
-        cabInvoiceGenerator.setUserSpecificInvoice(user2Rides, "User2");
+        cabInvoiceGenerator.setUserSpecificInvoice(user2Rides, "User2", UserCategory.NORMAL);
         Assert.assertEquals(new InvoiceSummary(2,37), cabInvoiceGenerator
                 .getUserInvoiceSummary("User1"));
+    }
+    @Test
+    public void givenUserCategory_PREMIUM_ShouldReturnPREMIUM(){
+        Ride[] user1Rides = { new Ride(3.0, 2),
+                new Ride(0.1, 1)};
+        cabInvoiceGenerator.setUserSpecificInvoice(user1Rides, "User1", UserCategory.NORMAL);
+        Ride []user2Rides = {new Ride(5.0, 2),
+                new Ride(6.1, 3)};
+        cabInvoiceGenerator.setUserSpecificInvoice(user2Rides, "User2", UserCategory.PREMIUM);
+        Assert.assertEquals(new InvoiceSummary(2,176.5), cabInvoiceGenerator
+                .getUserInvoiceSummary("User2"));
     }
 }
